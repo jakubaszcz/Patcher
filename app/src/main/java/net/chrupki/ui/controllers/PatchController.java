a68a725de60cd3a2e50a344f6343b06a5b39507f
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-01-04 00:27:39 +0100

    Add patch controller

---
+++ app/src/main/java/net/chrupki/ui/controllers/PatchController.java
@@ -0,0 +1,9 @@
+package net.chrupki.ui.controllers;
+
+import net.chrupki.model.PatchRequest;
+
+public class PatchController {
+    public void createPatch(PatchRequest request) {
+        // Request to PatchDAO
+    }
+}
