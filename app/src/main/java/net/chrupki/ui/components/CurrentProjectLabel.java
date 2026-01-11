7ea58e65aaff7760ab5e0509440f07b5bd10abe2
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-01-11 13:52:49 +0100

    Edit current project label

--- app/src/main/java/net/chrupki/ui/components/CurrentProjectLabel.java
+++ app/src/main/java/net/chrupki/ui/components/CurrentProjectLabel.java
@@ -1,25 +1,31 @@
 package net.chrupki.ui.components;
 
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import net.chrupki.app.AppData;
 
 public class CurrentProjectLabel extends VBox {
 
     private static final String BLANK = "No current project";
-    private final Label projectLabel = new Label();
+
+    private static final HBox view = new HBox();
 
     public CurrentProjectLabel() {
+        Label label = new Label(BLANK);
+        Button button = new Button("Edit project");
 
-        projectLabel.textProperty().bind(
-                Bindings.when(
-                                AppData.getPropertyCurrentProjectName().isNull()
-                        )
-                        .then(BLANK)
-                        .otherwise(AppData.getPropertyCurrentProjectName())
-        );
+        StringProperty currentProject = AppData.getPropertyCurrentProjectName();
 
-        this.getChildren().add(projectLabel);
+        view.visibleProperty().bind(currentProject.isNotNull().and(currentProject.isNotEmpty()));
+
+        label.textProperty().bind(Bindings.when(view.visibleProperty()).then(currentProject).otherwise(AppData.getPropertyCurrentProjectName()));
+
+        view.getChildren().addAll(label, button);
+
+        this.getChildren().addAll(view);
     }
 }
