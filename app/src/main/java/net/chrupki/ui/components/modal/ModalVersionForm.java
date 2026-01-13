212e8cf7814e0b82f49f9aa3c978120528d5c3ed
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-01-13 19:20:13 +0100

    feat(ui): create basic ui for version modal
    Every event has been linked to the controller as well.

--- app/src/main/java/net/chrupki/ui/components/modal/ModalVersionForm.java
+++ app/src/main/java/net/chrupki/ui/components/modal/ModalVersionForm.java
@@ -4,7 +4,35 @@ import javafx.scene.layout.VBox;
 
 public class ModalVersionForm extends VBox {
 
-    public ModalVersionForm() {
+    public ModalVersionForm(ProjectModel model, BiConsumer<Integer, String> onSave, Runnable onClose) {
+
+        setAlignment(Pos.CENTER);
+
+        visibleProperty().bind(model.getEditVersionProperty());
+        managedProperty().bind(model.getEditVersionProperty());
+
+
+        Label label = new Label();
+
+        TextField textField = new TextField();
+
+        textField.setPromptText("Enter new version name");
+
+        Button save = new Button("Save");
+
+        Button close = new Button("Close");
+
+        save.setOnAction(e -> {
+            onSave.accept(AppContext.versionContext().getId().get(), textField.getText());
+        });
+
+        close.setOnAction(e -> {
+            onClose.run();
+        });
+
+        label.textProperty().bind(AppContext.projectContext().getName());
+
+        getChildren().addAll(label, textField, save, close);
 
     }
 }
