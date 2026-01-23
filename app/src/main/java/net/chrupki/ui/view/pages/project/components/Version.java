4b5c7075fc2836509be54ce1bb4adc4c4dd6a9b7
jakub <jakub.szczucinski@epitech.eu>
2026-01-23 17:08:13 +0100

    feat(ui): add scroll area for Version area

--- app/src/main/java/net/chrupki/ui/view/pages/project/components/Version.java
+++ app/src/main/java/net/chrupki/ui/view/pages/project/components/Version.java
@@ -2,6 +2,7 @@ package net.chrupki.ui.view.pages.project.components;
 
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import net.chrupki.ui.controllers.HubController;
@@ -11,12 +12,22 @@ import net.chrupki.ui.view.pages.project.dto.VersionDTO;
 public class Version extends VBox {
 
     private final VBox list = new VBox();
+    private final ScrollPane scrollPane = new ScrollPane();
 
     public Version() {
 
         ObservableList<VersionDTO> versions = ProjectModel.getVersions();
 
-        VBox.setVgrow(list, Priority.ALWAYS);
+        scrollPane.setContent(list);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setPannable(true);
+
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+        scrollPane.getStyleClass().add("invisible-scroll");
+
+        VBox.setVgrow(scrollPane, Priority.ALWAYS);
 
         refresh(versions);
 
@@ -27,15 +38,20 @@ public class Version extends VBox {
         getStyleClass().add("project-panel");
         getChildren().addAll(
                 new VersionHeader(),
-                list);
+                scrollPane
+        );
     }
 
     private void refresh(ObservableList<VersionDTO> versions) {
         list.getChildren().clear();
 
         for (VersionDTO v : versions) {
-            list.getChildren().add(new VersionContainer(v, HubController.getVersionController()::selectVersion));
+            list.getChildren().add(
+                    new VersionContainer(
+                            v,
+                            HubController.getVersionController()::selectVersion
+                    )
+            );
         }
     }
-
 }
