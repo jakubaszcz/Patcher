09fa7e4a9eb16fc5ea5fea26443cd023879a5e13
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-01-29 19:06:21 +0100

    add(db): create dao for tag

---
+++ app/src/main/java/net/chrupki/database/dao/TagDAO.java
@@ -0,0 +1,67 @@
+package net.chrupki.database.dao;
+
+import net.chrupki.database.DatabaseHub;
+import net.chrupki.dto.TagDTO;
+import net.chrupki.dto.VersionDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TagDAO {
+    public static TagDTO insert(TagDTO tagDTO) {
+        String sql = "INSERT INTO tags (tag) VALUES (?)";
+
+        try (Connection conn = DatabaseHub.getInstance().getConnection();
+             PreparedStatement stmt =
+                     conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            stmt.setString(1, tagDTO.getName());
+            stmt.executeUpdate();
+
+            try (ResultSet rs = stmt.getGeneratedKeys()) {
+                if (rs.next()) {
+                    return new TagDTO(
+                            rs.getString("tag"),
+                            rs.getInt("id")
+                    );
+                }
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static List<TagDTO> all() {
+        List<TagDTO> result = new ArrayList<>();
+
+        String sql = """
+        SELECT id, tag
+        FROM tags
+        ORDER BY id ASC
+        """;
+
+        try (Connection conn = DatabaseHub.getInstance().getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                result.add(new TagDTO(
+                        rs.getString("tag"),
+                        rs.getInt("id")
+                ));
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return result;
+    }
+}
