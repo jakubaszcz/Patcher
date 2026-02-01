2f72d31f92a8bf41c0c43042e0c267dd984ab6e9
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-02-01 19:45:06 +0100

    add(controller): create tag controller

--- app/src/main/java/net/chrupki/ui/controllers/files/TagController.java
+++ app/src/main/java/net/chrupki/ui/controllers/files/TagController.java
@@ -1,4 +1,24 @@
 package net.chrupki.ui.controllers.files;
 
+import net.chrupki.model.HubModel;
+import net.chrupki.project.services.HubService;
+import net.chrupki.ui.model.GlobalModel;
+
+import java.io.IOException;
+
 public class TagController {
+
+    public void load() {
+        try {
+            GlobalModel.getTags().setAll(
+                    HubService.getTagService().fetch(HubModel.projectModel().getName().get())
+            );
+        } catch (IOException e) {
+
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
