05b4b79c6937cf9365f280d282dc98cd3bbb2133
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-02-01 19:41:32 +0100

    add(service): create service for tag

--- app/src/main/java/net/chrupki/project/services/files/TagService.java
+++ app/src/main/java/net/chrupki/project/services/files/TagService.java
@@ -1,4 +1,21 @@
 package net.chrupki.project.services.files;
 
+import net.chrupki.database.dao.TagDAO;
+import net.chrupki.database.dao.VersionDAO;
+import net.chrupki.dto.TagDTO;
+import net.chrupki.dto.VersionDTO;
+
+import java.util.List;
+
 public class TagService {
+
+    public List<TagDTO> fetch(String projectName) throws Exception {
+
+        if (projectName == null || projectName.isBlank()) {
+            return List.of();
+        }
+
+        return TagDAO.all();
+    }
+
 }
